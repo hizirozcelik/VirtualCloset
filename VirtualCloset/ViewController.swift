4f2585ff3819506f3936fef005779eb710613126
hizirozcelik <ozcelikh@sheridancollege.ca>
2023-03-02 17:20:59 -0500

    first Camera

--- VirtualCloset/ViewController.swift
+++ VirtualCloset/ViewController.swift
@@ -9,12 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var imageView : UIImageView!
+    @IBOutlet var button : UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imageView.backgroundColor = .secondarySystemBackground
+        
+        button.backgroundColor = .systemBlue
+        button.setTitle("Take Picture", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        
     }
+    
+    @IBAction func didButtonClicked(){
+        
+        let picker = UIImagePickerController()
+        picker.sourceType = .camera
+        picker.delegate = self
+        present(picker, animated: true)
+        
+    }
+}
 
-
+extension ViewController: UIImagePickerControllerDelegate,
+                          UINavigationControllerDelegate {
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        
+        picker.dismiss(animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        picker.dismiss(animated: true, completion: nil)
+        
+        guard let image = info[UIImagePickerController.InfoKey.originalImage] as?
+                UIImage else{
+            return
+        }
+        imageView.image = image
+    }
 }
 
